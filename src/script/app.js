28a887d4439b5c3729a27df1dedfd7e71df6b271
Truong Le <lntruong797@gmail.com>
2019-07-18 21:09:57 +0700

    Create Repo

---
+++ src/script/app.js
@@ -0,0 +1,7 @@
+'use strict'
+
+(function(){
+    angular.module('app', ['app2']);
+
+    angular.module('app2', []);
+})();
---
+++ src/script/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+(function(){
+    angular.module('app')
+    .controller('IndexCtr', function($scope){
+
+    });
+})();
